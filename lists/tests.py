81bf19baf9c1f3cbdfd0844ef055b483958617b0
hyunji92 <chunghj09@gmail.com>
2017-02-17 16:59:13 +0900

    minimum reponse html basic view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,9 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
+
 
 # Create your tests here.
 #class SmokeTest(TestCase):   
