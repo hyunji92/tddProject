abaa7fd8c52e05745cef403f424d7d045d0c4555
hyunji92 <chunghj09@gmail.com>
2017-02-18 12:13:48 +0900

    functional test ,check add item list

--- functional_test.py
+++ functional_test.py
@@ -18,19 +18,34 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         # 웹 페이지 타이틀과 헤더가 'To-do'를 표시하고있다.
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
         # 테스트 어설션을 만들기 위해, assert 대신에 self.assertIn 사용
-        self.fail('Finish the test!')
-        # 강제적 테스트 실패 
-
+        self.assertIn('To-Do', header_text)
+        
         # 바로 작업을 추가하기로 한다.
+        inputbox =  self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
 
-        # "세일 상품 사기" 라고 텍스트 상자에 입력한다
+        # "세일 상품 사기" 라고 텍스트 상자에 입력한다. 
+        # 델라의 취미는 날치 잡이용 그물을 만드는 것이다.
+        inputbox.send_keys('세일상품 사기')
 
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에 
         # "1: 세일 상품 사기" 아이템이 추가된다.
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue (
+            any(row.text == '1: 세일상품 사기' for row in rows),
+        )
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
         # 다시 "세일 상품 사고 사용하기" 라고 입력한다.
+        self.fail('Finish the test!!!')
         
         # 페이지는 다시 갱신되고, 두 개의 아이템이 목록에 보인다
         # 델라는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
